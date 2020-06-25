1d5fe5529e4da790739259ece2f69babaa9e6278
Yor Name <hollisjamison@gmail.com>
2020-06-25 19:38:19 -0400

    Added class work loop

--- loops-object-es6.js
+++ loops-object-es6.js
@@ -2,7 +2,10 @@
 // array data type has forEach()
 // for loop
 // while loop 
-
+for (let i = 0; i <= 10; i++) {
+  // eslint-disable-next-line no-console
+  console.log('Loop number ' + i)
+}
 
 // introducing arrow functions
 
